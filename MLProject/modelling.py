2654a892242d6be3af4c3715963b5f0f0e1d5032
akhmad-ardi <akhmad.ardi1711@gmail.com>
2025-07-06 15:38:57 +0800

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,45 @@
+import sys
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Autolog semua parameter dan metrics
+mlflow.sklearn.autolog()
+
+# Load Dataset
+
+path = "./Predict_Student_Performance_preprocessing"
+
+df_train = pd.read_csv(f"{path}/train.csv")
+df_test = pd.read_csv(f"{path}/test.csv")
+
+X_train = df_train.drop(columns=['Grades'])
+y_train = df_train["Grades"]
+
+X_test = df_test.drop(columns=['Grades'])
+y_test = df_test["Grades"]
+
+n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 150
+max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 15
+
+with mlflow.start_run():
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=X_train[0:5]
+    )
+
+    model.fit(X_train, y_train)
+
+    mse = mean_squared_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+
+    print(f"Mean Squared Score: {mse}")
+    print(f"R-Squared Score: {r2}")
